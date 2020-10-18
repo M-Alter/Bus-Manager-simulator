d37b00c8e4943101ea9743fd94c8c883f69a08fa
PERSO\Inon.bezalel <inon.bezalel@personetics.com>
2020-10-18 22:43:58 +0300

    Accoring to steps prase 6 steps 3, added a new static partial void

---
+++ dotnet5781_00_2131_1146/Program1146.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_00_2131_1146
+{
+    partial class Program
+    {
+        static partial void Welcome1146()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
