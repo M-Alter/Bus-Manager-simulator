97983d54d4984f3fa3ef6bb199b480fa0bfef578
Menachem Alter <alter.menachem@gmail.com>
2020-10-18 22:28:18 +0300

    initial code + Welcome2131

---
+++ dotnet5781_00_2131_1146/Program2131.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_00_2131_1146
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2131();
+            Welcome1146();
+            Console.ReadKey();
+        }
+
+
+        static partial void Welcome1146();
+
+        private static void Welcome2131()
+        {
+            Console.WriteLine("Enter yout name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first conosole application", name);
+        }
+    }
+}
