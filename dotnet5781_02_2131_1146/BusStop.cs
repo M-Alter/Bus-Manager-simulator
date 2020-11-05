3fccc4add29f0359007ba8950386f4933995e504
PERSO\Inon.bezalel <inon.bezalel@personetics.com>
2020-11-06 00:07:16 +0200

    Add to BusStop list for all busess Edited constractor BusStopRoute

--- dotnet5781_02_2131_1146/BusStop.cs
+++ dotnet5781_02_2131_1146/BusStop.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace dotnet5781_02_2131_1146
 {
-    class BusStop
+    public class BusStop
     {
         private const int MAX_STOP_NUMBER = 1000000;
         private const int MIN_LAT = -90;
         private const int MAX_LAT = 90;
         private const int MIN_LON = -180;
         private const int MAX_LON = 180;
-
 
         public readonly int stopNumber;
         public readonly double Latitude;
@@ -25,11 +24,14 @@ namespace dotnet5781_02_2131_1146
             {
                 throw new ArgumentException("Number is longer than 6 digits");
             }
+            // Check if this stop number alredy exist
+            if (AllStopsNumbers.Contains(number))
+                throw new ArgumentException(string.Format("Stop {0} already existed", number));
             stopNumber = number;
+            AllStopsNumbers.Add(number);
             Random r = new Random(DateTime.Now.Millisecond);
             Latitude = r.NextDouble() * (33.3 - 31) + 31;
             Longitude = r.NextDouble() * (35.5 - 34.3) + 34.3;
-
         }
 
         public string Address   { get; set; }
@@ -38,5 +40,6 @@ namespace dotnet5781_02_2131_1146
         {
             return String.Format("Bus Station Code: {0}, {1}°N {2}°E", stopNumber, Latitude, Longitude);
         }
+        private static List<int> AllStopsNumbers = new List<int>();
     }
 }
---
+++ dotnet5781_02_2131_1146/BusStopRoute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_02_2131_1146
+{
+    public class BusStopRoute : BusStop
+    {
+        public BusStopRoute(int number, TimeSpan time, double distance) : base(number) 
+        {
+            TravelTime = time;
+            TravelDistance = distance;
+        }
+        //public BusStopRoute(BusStop busstop) : base(busstop.stopNumber) { }
+             
+        public TimeSpan TravelTime { get; set; }
+        public double TravelDistance { get; set; }
+        
+       
+
+    }
+}
