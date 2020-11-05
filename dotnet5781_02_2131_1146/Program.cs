06e00f6e12e65955612cf18e42fa1f74323cc8f1
Menachem Alter <alter.menachem@gmail.com>
2020-11-05 22:24:09 +0200

    created project 02

---
+++ dotnet5781_02_2131_1146/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_02_2131_1146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
