dd94c3ada5c3a73a24005e443777937787ee32ae
PERSO\Inon.bezalel <inon.bezalel@personetics.com>
2020-11-10 19:36:59 +0200

    changes in busStop

--- dotnet5781_02_2131_1146/busStopRoute.cs
+++ dotnet5781_02_2131_1146/busStopRoute.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace dotnet5781_02_2131_1146
 {
-    class BusStopRoute : BusStop
+    public class BusStopRoute : BusStop
     {
-        public BusStopRoute(int number) : base(number) { }
+        public BusStopRoute(int number, TimeSpan time, double distance) : base(number) 
+        {
+            TravelTime = time;
+            TravelDistance = distance;
+        }
+        //public BusStopRoute(BusStop busstop) : base(busstop.stopNumber) { }
+             
         public TimeSpan TravelTime { get; set; }
         public double TravelDistance { get; set; }
         
