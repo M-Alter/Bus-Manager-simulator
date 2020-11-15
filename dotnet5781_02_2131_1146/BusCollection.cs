3f7585a2e7474610d68b60f1b426a70b0f095509
Menachem Alter <alter.menachem@gmail.com>
2020-11-15 21:27:29 +0200

    BusCollection.Remove

--- dotnet5781_02_2131_1146/BusCollection.cs
+++ dotnet5781_02_2131_1146/BusCollection.cs
@@ -31,6 +31,78 @@ namespace dotnet5781_02_2131_1146
             BusLines.Add(busLine);
         }
 
+        public void RemovLine()
+        {
+            int input, input2;
+            bool success;
+            Console.WriteLine("This is all the line in this compeny:");
+            foreach (var item in BusLines)
+                 Console.WriteLine(item.LineNumber);
+            Console.WriteLine("\nWhich line do you want to remove?");
+            success = int.TryParse(Console.ReadLine(), out input);
+            if (!success)
+                throw new BusException("Wrong input");
+            if (lines.ContainsKey(input) && lines[input] == 2)
+            {
+                int i = 1;
+                BusLine b1 = default(BusLine), b2 = default(BusLine);
+                Console.WriteLine("There is two ways for this line, which one do you want?");
+                foreach (var item in BusLines)
+                {
+                    if (item.LineNumber == input)
+                    {
+                        Console.WriteLine($"{i}: {item}");
+                        if (i == 1)
+                        {
+                            b1 = item;
+                            i++;
+                        }
+                        else
+                            b2 = item;
+                    }
+                }
+                success = int.TryParse(Console.ReadLine(), out input2);
+                if (success)
+                    success = (input2 == 1 || input2 == 2);
+                if (!success)
+                    throw new BusException("Wrong input");
+                if (input2 == 1)
+                {
+                    BusLines.Remove(b1);
+                }
+                else
+                {
+                    BusLines.Remove(b2);
+                }
+                lines[input] = 1;
+            }
+            else if (lines.ContainsKey(input) && lines[input] == 1)
+            {
+                BusLines.Remove(BusLines[input]);
+                lines.Remove(input);
+            }
+            else
+                throw new BusException("This line doesn't exist in this company");
+        }
+
+        public BusLine this[int index]
+        {
+            get
+            {
+                foreach (var item in BusLines)
+                    if (item.LineNumber == index)
+                        return item;
+                throw new BusException("This item doesn't exist");
+            }
+        }
+
+        public void Print() 
+        {
+            foreach (var item in BusLines)
+            {
+                Console.WriteLine(item);
+            }
+        }
 
         public void PrintAreas()
         {
--- dotnet5781_02_2131_1146/busStopRoute.cs
+++ dotnet5781_02_2131_1146/busStopRoute.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace dotnet5781_02_2131_1146
 {
-    class BusStopRoute
+    public class BusStopRoute
     {
         public BusStopRoute(BusStop stop, TimeSpan time, double distance)
         {
