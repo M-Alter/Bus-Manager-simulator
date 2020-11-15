a304764e12973a650c46a17b96641b125c155a54
Menachem Alter <alter.menachem@gmail.com>
2020-11-15 17:56:36 +0200

    Bus EXception

---
+++ dotnet5781_02_2131_1146/BusExcexption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet5781_02_2131_1146
+{
+    class BusExcexption : Exception
+    {
+        BusExcexption(string info) : base(info);
+
+        public override string Message => base.Message;
+    }
+
+    
+}
