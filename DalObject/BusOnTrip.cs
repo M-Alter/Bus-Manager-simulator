4e3a4df6f2fb7e600414e34b9656463ecc837a3d
inonbezalel <inonbezalel@gmail.com>
2020-12-22 21:04:45 +0200

    class BusOnTrip

---
+++ DalObject/BusOnTrip.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    class BusOnTrip
+    {
+        #region Fields
+        private int id;
+        private int licenseNum;
+        private int lineId;
+        private TimeSpan plannedTakeOff;
+        private TimeSpan actualTakeOff;
+        private int prevStation;
+        private TimeSpan prevStationAt;
+        private TimeSpan nextStationAt;
+        #endregion
+
+        #region Properties
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public int LicenseNum
+        {
+            get { return licenseNum; }
+            set { licenseNum = value; }
+        }
+
+
+        public int LineId
+        {
+            get { return lineId; }
+            set { lineId = value; }
+        }
+
+
+        public TimeSpan PlannedTakeOff
+        {
+            get { return plannedTakeOff; }
+            set { plannedTakeOff = value; }
+        }
+
+
+        public TimeSpan ActualTakeOff
+        {
+            get { return actualTakeOff; }
+            set { actualTakeOff = value; }
+        }
+
+
+        public int PrevStation
+        {
+            get { return prevStation; }
+            set { prevStation = value; }
+        }
+
+
+        public TimeSpan PrevStationAt
+        {
+            get { return prevStationAt; }
+            set { prevStationAt = value; }
+        }
+
+
+        public TimeSpan NextStationAt
+        {
+            get { return nextStationAt; }
+            set { nextStationAt = value; }
+        }
+        #endregion
+
+    }
+}
