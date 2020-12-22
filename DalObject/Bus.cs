a4fb7fc94065b08c8fbb6f21e66a30bf6c545f3d
inonbezalel <inonbezalel@gmail.com>
2020-12-22 20:46:32 +0200

    class Bus

---
+++ DalObject/Bus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    class Bus
+    {
+        private int licenseNum;
+        private DateTime fromDate;
+        private double totalTrip;
+        private double fuelRemain;
+        private BusStatus status;
+
+        public int LicenseNum
+        {
+            get { return licenseNum; }
+            set { licenseNum = value; }
+        }
+
+
+        public DateTime FromDate
+        {
+            get { return fromDate; }
+            set { fromDate = value; }
+        }
+
+
+        public double TotalTrip
+        {
+            get { return totalTrip; }
+            set { totalTrip = value; }
+        }
+
+
+        public double FuelRemain
+        {
+            get { return fuelRemain; }
+            set { fuelRemain = value; }
+        }
+
+
+        public BusStatus Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+
+
+    }
+}
