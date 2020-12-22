efc18bd7bbab558642364fa0cf09c7db8658ea92
inonbezalel <inonbezalel@gmail.com>
2020-12-22 21:14:04 +0200

    class Station

---
+++ DalObject/Station.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    class Station
+    {
+        private int code;
+        private string name;
+        private int longitude;
+        private int lattitude;
+
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public int Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+
+
+        public int Lattitude
+        {
+            get { return lattitude; }
+            set { lattitude = value; }
+        }
+
+
+    }
+}
