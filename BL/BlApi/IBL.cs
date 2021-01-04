4ef2002f104fe541ee9a0344ea833e29eb225266
inonbezalel <inonbezalel@gmail.com>
2021-01-04 15:38:55 +0200

    Check

--- BL/BlApi/IBL.cs
+++ BL/BlApi/IBL.cs
@@ -9,16 +9,20 @@ namespace BLAPI
 {
     public interface IBL
     {
+        #region Bus
         Bus GetBus(int license);
         IEnumerable<Bus> GetAllBuses();
         IEnumerable<Bus> GetAllBusesThat(Predicate<Bus> predicate);
         bool AddBus(Bus bus);
+
+        #endregion
         Station GetStation(int id);
         IEnumerable<Station> GetAllStations();
         bool AddStation(Station station);
         Line GetLine(int id);
         IEnumerable<Line> GetAllLines();
         bool AddLine(Line line);
+        bool RemoveStationFromLine(Line line, int stationToRemove);
         IEnumerable<string> GetAllUserNames();
         bool ValidateUser(string userName, string password);
 
