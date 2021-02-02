0f50bec2a1290de4e686543524bd04cc82d7d1bc
Menachem <alter.menachem@gmail.com>
2021-02-02 02:54:51 +0200

    comments

--- BL/BO/LineStation.cs
+++ BL/BO/LineStation.cs
@@ -8,10 +8,26 @@ namespace BO
 {
     public class LineStation
     {
+        /// <summary>
+        /// index in the line
+        /// </summary>
         public int Index { get; set; }
+
+        /// <summary>
+        /// station number
+        /// </summary>
         public int Station { get; set; }
+        /// <summary>
+        /// display name of the station
+        /// </summary>
         public string StationName { get; set; }
+        /// <summary>
+        /// time to the next station 
+        /// </summary>
         public TimeSpan? TimeToNext { get; set; }
+        /// <summary>
+        /// distance to the next station
+        /// </summary>
         public double? Distance { get; set; }
     }
 }
