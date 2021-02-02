49d5ab38b706bc2c264ee4e88fb93cf114df6731
Menachem <alter.menachem@gmail.com>
2021-02-02 02:53:22 +0200

    comments

--- BL/BO/Line.cs
+++ BL/BO/Line.cs
@@ -19,42 +19,74 @@ namespace BO
         #endregion
 
         #region Properties
+        /// <summary>
+        /// personal id of the line
+        /// </summary>
         public int PersonalId
         {
             get { return personalId; }
             set { personalId = value; }
         }
       
+        /// <summary>
+        /// display line number of the line
+        /// </summary>
         public int LineNumber
         {
             get { return lineNumber; }
             set { lineNumber = value; }
         }
 
+        /// <summary>
+        /// area that the bus operates
+        /// </summary>
         public Enums.Areas Area
         {
             get { return areas; }
             set { areas = value; }
         }
 
+        /// <summary>
+        /// first station number of the line
+        /// </summary>
         public int FirstStation
         {
             get { return firstStation; }
             set { firstStation = value; }
         }
 
+        /// <summary>
+        /// name of the first station
+        /// </summary>
         public string FirstStationName { get; set; }
 
+        /// <summary>
+        /// last station number
+        /// </summary>
         public int LastStation
         {
             get { return lastStation; }
             set { lastStation = value; }
         }
 
+        /// <summary>
+        /// name of the last station
+        /// </summary>
         public string LastStationName { get; set; }
+
+        /// <summary>
+        /// collection of the stations in this line
+        /// </summary>
         public IEnumerable<LineStation> Stations { get; set; }
+
+        /// <summary>
+        /// collection of all the timing of the line
+        /// </summary>
         public IEnumerable<TimeSpan> Timing { get; set; }
 
+        /// <summary>
+        /// false if inactive 
+        /// </summary>
         public bool IsActive
         {
             get { return isActive; }
