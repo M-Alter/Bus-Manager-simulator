20fd06d4d1c520d40e4dca7175fe9e9770561db7
Menachem <alter.menachem@gmail.com>
2021-02-02 02:46:29 +0200

    comments

--- BL/BO/AdjacentStations.cs
+++ BL/BO/AdjacentStations.cs
@@ -16,27 +16,46 @@ namespace BO
         #endregion
 
         #region Properties
+        /// <summary>
+        /// first station in the pair
+        /// </summary>
         public int Station1
         {
             get { return station1; }
             set { station1 = value; }
         }
 
+        /// <summary>
+        /// name of the first station
+        /// </summary>
         public string Station1Name { get; set; }
+
+        /// <summary>
+        /// name of the second station
+        /// </summary>
         public string Station2Name { get; set; }
 
+        /// <summary>
+        /// second station
+        /// </summary>
         public int Station2
         {
             get { return station2; }
             set { station2 = value; }
         }
 
+        /// <summary>
+        /// distance to the next station
+        /// </summary>
         public double Distance
         {
             get { return distance; }
             set { distance = value; }
         }
 
+        /// <summary>
+        /// time to the next station
+        /// </summary>
         public TimeSpan Time
         {
             get { return time; }
