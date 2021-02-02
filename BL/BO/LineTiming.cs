67996d0a4a89bb0c27acff31292ee20516a7a051
Menachem <alter.menachem@gmail.com>
2021-02-02 02:58:06 +0200

    comments

--- BL/BO/LineTiming.cs
+++ BL/BO/LineTiming.cs
@@ -7,14 +7,43 @@ using System.Threading.Tasks;
 namespace BO
 {
     public class LineTiming
-    {        
+    {       
+        /// <summary>
+        /// internal counter for identity reasons
+        /// </summary>
         private static int counter = 0;
+
+        /// <summary>
+        /// id of the lineTiming
+        /// </summary>
         public int ID;
+
+        /// <summary>
+        /// c'tor that gives the ID a value
+        /// </summary>
         public LineTiming() => ID = ++counter;
-        public TimeSpan TripStart { get; set; }        
-        public int LineId { get; set; }        
-        public int LineNumber { get; set; }        
-        public string LastStation { get; set; }        
+
+        
+        //public TimeSpan TripStart { get; set; }
+
+        /// <summary>
+        /// id of the line
+        /// </summary>
+        public int LineId { get; set; }       
+        
+        /// <summary>
+        /// display number of the line
+        /// </summary>
+        public int LineNumber { get; set; }     
+        
+        /// <summary>
+        /// final station of the line
+        /// </summary>
+        public string LastStation { get; set; }
+
+        /// <summary>
+        /// time to start the trip
+        /// </summary>
         public TimeSpan Timing { get; set; }
     }
 }
